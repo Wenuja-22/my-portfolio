4534f174c2e22ac907e4f587ab9416f5af1c23f1
Wenuja-22 <wenuja5570@gmail.com>
2026-07-01 06:26:14 +0530

    Add Portfolio Website Files
    uploaded index.html and related assets for portfolio site.

---
+++ W-0.js
@@ -0,0 +1,60 @@
+    document.addEventListener("DOMContentLoaded", () => {
+        const fills = document.querySelectorAll(".fill");
+
+        // When the skills section is visible, animate bars
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    fills.forEach(fill => {
+                        const percent = fill.getAttribute("data-percent");
+                        fill.style.width = percent + "%";
+                    });
+                }
+            });
+        }, { threshold: 0.5 });
+
+        observer.observe(document.querySelector("#skills"));
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const fills = document.querySelectorAll(".fill");
+        const percentages = document.querySelectorAll(".percentage");
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    fills.forEach((fill, index) => {
+                        const percent = parseInt(fill.getAttribute("data-percent"));
+                        const target = parseInt(percentages[index].getAttribute("data-target"));
+
+                        if (!percentages[index].classList.contains("done")) {
+                            percentages[index].classList.add("done");
+
+                            fill.style.width = percent + "%";
+
+                            // Animate Percenta Counter
+                        
+                            let count = 0;
+                            const interval = setInterval(() => {
+                                if (count < target) {
+                                    count++;
+                                    percentages[index].textContent = count + "%";
+                                }else{
+                                    clearInterval(interval);
+                                }
+                            }, 20);
+                        }    
+                });
+            }
+        });
+    }, {threshold: 0.3});
+
+    observer.observe(document.querySelector("#skills"));
+    });
+
+    
+    const thankyou = new URLSearchParams(window.location.search);
+        if (thankyou.has('success')) {
+            document.getElementById('thankyou-message').style.display='block'
+        };
+
